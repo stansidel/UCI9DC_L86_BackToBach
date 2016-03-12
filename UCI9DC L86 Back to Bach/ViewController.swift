9b24c17348aa9ef23b7f20b576f856a1e68b02ea
Станислав Сидельников <sidelnikov.stanislav@gmail.com>
2016-03-12 21:26:12 +0500

    Initial version

--- UCI9DC L86 Back to Bach/ViewController.swift
+++ UCI9DC L86 Back to Bach/ViewController.swift
@@ -7,19 +7,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var playbackSlider: UISlider!
+    @IBOutlet weak var volumeSlider: UISlider!
+    private var player: AVAudioPlayer?
+    private var timer: NSTimer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupPlayer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func setupPlayer() {
+        if let filePath = NSBundle.mainBundle().pathForResource("secret_garden", ofType: "mp3") {
+            do {
+                player = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: filePath))
+            } catch let error {
+                NSLog("Unable to initialize player. Error: \(error)")
+                return
+            }
+            player?.volume = volumeSlider.value
+        } else {
+            NSLog("Unable to initialize file path")
+        }
+    }
 
 
+    @IBAction func playPressed(sender: UIButton) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updatePlaybackSlider", userInfo: nil, repeats: true)
+        player?.play()
+    }
+    
+    @IBAction func pausePressed(sender: UIButton) {
+        timer?.invalidate()
+        player?.pause()
+    }
+    
+    @IBAction func stopPressed(sender: UIButton) {
+        timer?.invalidate()
+        player?.stop()
+        player?.currentTime = 0
+        playbackSlider.value = 0
+    }
+    
+    @IBAction func changeVolume(sender: UISlider) {
+        player?.volume = sender.value
+    }
+    
+    func updatePlaybackSlider() {
+        if let player = player where player.duration > 0 {
+            playbackSlider.value = Float(player.currentTime) / Float(player.duration)
+        } else {
+            playbackSlider.value = 0
+        }
+    }
+    
+    @IBAction func playbackMove(sender: UISlider) {
+        player?.currentTime = Double(Float(player?.duration ?? 0) * playbackSlider.value)
+    }
 }
 
