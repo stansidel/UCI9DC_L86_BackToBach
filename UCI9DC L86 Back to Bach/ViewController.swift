e650de2638fbbd8b73948799f4ad998701235f41
Станислав Сидельников <sidelnikov.stanislav@gmail.com>
2016-03-12 20:24:22 +0500

    Initial Commit

---
+++ UCI9DC L86 Back to Bach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCI9DC L86 Back to Bach
+//
+//  Created by Stanislav Sidelnikov on 3/12/16.
+//  Copyright © 2016 Stanislav Sidelnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
